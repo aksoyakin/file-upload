36f19eefb1190aad1a45b45e6d78656620e4fa85
AKIN AKSOY <dev@aksoyakin.com>
2025-03-14 10:42:13 +0300

    test

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -5,10 +5,13 @@ const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     logger.error(`${err.message} - ${err.stack}`);
 
+    /*
     res.status(statusCode).json(responseFormatter.error(
         err.message || 'Sunucu hatası',
         process.env.NODE_ENV === 'production' ? null : err.stack
     ));
+
+     */
 };
 
 export default errorHandler;
