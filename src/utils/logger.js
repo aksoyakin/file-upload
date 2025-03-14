ea4b6f05089af68311285fbe1ba01b7fba54717e
AKIN AKSOY <dev@aksoyakin.com>
2025-03-14 15:47:56 +0300

    log

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const logsDir = "./tmp/logs";
+const logsDir = "/tmp/logs";
 //const logsDir = path.join(__dirname, '..', '..', 'logs');
 
 // Loglar için klasör oluşturma
